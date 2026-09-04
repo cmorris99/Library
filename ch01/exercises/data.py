c1bd5fccdd4b8edeaf407100410110eca50762ea
Chris Morris <cmorris11@binghamton.edu>
2026-09-04 16:49:52 -0400

    data.py comp

---
+++ ch01/exercises/data.py
@@ -0,0 +1,22 @@
+print("10*5#Expected Answer:50", 10*5)
+print("10**2#Expected Answer:20", 10**2)
+print("15/10#Expected Answer:1.5", 15/10)
+print("15//10#Expected Answer:1", 15//10)
+print("-15//10#Expected Answer:-2", -15//10)
+print("15%10#Expected Answer:5", 15%10)
+print("10%15#Expected Answer:10", 10%15)
+print("10%10#Expected Answer:0", 10%10)
+print("0%10#Expected Answer:0", 0%10)
+print("10/15#Expected Answer:0.666", 10/15)
+
+##OUTPUT
+#10*5#Expected Answer:50 50
+#10**2#Expected Answer:20 100
+#15/10#Expected Answer:1.5 1.5
+#5//10#Expected Answer:1 1
+#-15//10#Expected Answer:-2 -2
+#15%10#Expected Answer:5 5
+#10%15#Expected Answer:10 10
+#10%10#Expected Answer:0 0
+#0%10#Expected Answer:0 0
+#10/15#Expected Answer:0.666 0.6666666666666666
